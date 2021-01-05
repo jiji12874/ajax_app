6aec3aa53aabd2eca1cc5446b1b152dd2e0c1fdf
jiji12874 <jiji12874jiji@gmail.com>
2021-01-05 22:06:01 +0900

    Revert "既読機能のサーバーサイド実装"
    This reverts commit 6697d0c25885a677a5b170fc700275b8b0c58b62.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,17 +10,5 @@ class PostsController < ApplicationController
  def create
   Post.create(content: params[:content])
   redirect_to action: :index
-
-  def checked
-    post = Post.find(params[:id])
-    if post.checked 
-      post.update(checked: false)
-    else
-      post.update(checked: true)
-    end
-
-    item = Post.find(params[:id])
-    render json: { post: item }
-  end
 end
 end
